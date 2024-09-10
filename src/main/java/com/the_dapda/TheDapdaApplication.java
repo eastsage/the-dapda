ca466474479d4402db9c4564f1f0f971b77792db
jun10920 <abide0206@naver.com>
2024-09-10 18:18:45 +0900

    환경세팅

---
+++ src/main/java/com/the_dapda/TheDapdaApplication.java
@@ -0,0 +1,13 @@
+package com.the_dapda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheDapdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheDapdaApplication.class, args);
+	}
+
+}
