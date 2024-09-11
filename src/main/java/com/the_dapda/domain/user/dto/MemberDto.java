fcf47576f1bedb7201f630234aa6aaace6df5bf9
JungYujin <ujin8523@naver.com>
2024-09-11 10:41:33 +0900

    Member Entity생성

---
+++ src/main/java/com/the_dapda/domain/user/dto/MemberDto.java
@@ -0,0 +1,12 @@
+package com.the_dapda.domain.user.dto;
+
+import java.time.LocalDateTime;
+
+public class MemberDto {
+
+    private int member_id;
+    private String id;
+    private String password;
+    private String nickname;
+    private LocalDateTime createdDate;
+}
