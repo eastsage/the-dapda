60161462e785b0db503f758db69de55af66708da
acs0209 <annjae4676@naver.com>
2024-09-13 15:39:08 +0900

    Feat: 일기 저장 시 날짜와 유저 저장 구현

--- src/main/java/com/the_dapda/domain/diary/controller/DiaryCommandController.java
+++ src/main/java/com/the_dapda/domain/diary/controller/DiaryCommandController.java
@@ -3,14 +3,21 @@ package com.the_dapda.domain.diary.controller;
 import com.the_dapda.domain.diary.dto.request.DiarySaveRequest;
 import com.the_dapda.domain.diary.dto.response.DiaryDeleteResponse;
 import com.the_dapda.domain.diary.dto.response.DiarySaveResponse;
+import com.the_dapda.domain.diary.entity.Date;
 import com.the_dapda.domain.diary.service.command.DiaryCommandService;
+import com.the_dapda.domain.user.entity.User;
 import com.the_dapda.global.response.ResponseCode;
 import com.the_dapda.global.response.ResponseForm;
+import com.the_dapda.global.session.SessionConst;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/diaries")
@@ -20,8 +27,21 @@ public class DiaryCommandController {
 
     // 일기 저장
     @PostMapping
-    public ResponseEntity<ResponseForm> saveDiary(@RequestBody DiarySaveRequest saveRequest) {
-        DiarySaveResponse diarySaveResponse = diaryCommandService.saveDiary(saveRequest);
+    public ResponseEntity<ResponseForm> saveDiary(
+            @RequestBody DiarySaveRequest saveRequest,
+            HttpServletRequest request) {
+
+        HttpSession session = request.getSession();
+        int year = (Integer) session.getAttribute(String.valueOf(SessionConst.YEAR));
+        int month = (Integer) session.getAttribute(String.valueOf(SessionConst.MONTH));
+        log.info("year {}", year);
+        log.info("month {}", month);
+
+        User user = (User) session.getAttribute("user");
+        log.info("user {}", user);
+
+        saveRequest.setDate(new Date(year, month));
+        DiarySaveResponse diarySaveResponse = diaryCommandService.saveDiary(saveRequest, user);
 
         return diarySaveResponse != null ?
                 ResponseEntity.ok(ResponseForm.of(ResponseCode.EXAMPLE_SUCCESS, diarySaveResponse)) :
--- src/main/java/com/the_dapda/domain/diary/service/command/DiaryCommandService.java
+++ src/main/java/com/the_dapda/domain/diary/service/command/DiaryCommandService.java
@@ -3,10 +3,11 @@ package com.the_dapda.domain.diary.service.command;
 import com.the_dapda.domain.diary.dto.request.DiarySaveRequest;
 import com.the_dapda.domain.diary.dto.response.DiaryDeleteResponse;
 import com.the_dapda.domain.diary.dto.response.DiarySaveResponse;
+import com.the_dapda.domain.user.entity.User;
 
 public interface DiaryCommandService {
 
-    DiarySaveResponse saveDiary(DiarySaveRequest saveRequest);
+    DiarySaveResponse saveDiary(DiarySaveRequest saveRequest, User user);
 
     DiaryDeleteResponse deleteDiary(Long diaryId);
 }
