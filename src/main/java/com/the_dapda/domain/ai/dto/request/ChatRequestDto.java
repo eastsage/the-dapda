9fa493b78ddb8c6962350a242a8538c7e3037800
jun10920 <abide0206@naver.com>
2024-09-13 14:14:02 +0900

    Refactor: builder 패턴으로 수정

--- src/main/java/com/the_dapda/domain/ai/dto/request/ChatRequestDto.java
+++ src/main/java/com/the_dapda/domain/ai/dto/request/ChatRequestDto.java
@@ -1,16 +1,12 @@
 package com.the_dapda.domain.ai.dto.request;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class ChatRequestDto {
     private String question; // 요청사항
     private String content; // 요청사항
     private String tfMode; // 요청사항
-
-    public ChatRequestDto(String question, String content, String TFmode) {
-        this.question = question;
-        this.content = content;
-        this.tfMode = TFmode;
-    }
 }
