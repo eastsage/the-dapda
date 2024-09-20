af0e085074dcee3e3165aa3d96a5b36c6d4e21c7
eastsage <dpwk8@naver.com>
2024-09-20 11:11:19 +0900

    Feat: main & view-diary 연결 및 삭제 기능 구현

--- src/main/java/com/the_dapda/domain/diary/repository/DiaryRepository.java
+++ src/main/java/com/the_dapda/domain/diary/repository/DiaryRepository.java
@@ -1,14 +1,19 @@
 package com.the_dapda.domain.diary.repository;
 
+import com.the_dapda.domain.diary.entity.Date;
 import com.the_dapda.domain.diary.entity.Diary;
+import com.the_dapda.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
 
     @Query("select d from Diary d where d.date.year = :year and d.date.month = :month and d.user.userId = :userId ORDER BY d.date.day")
     List<Diary> getMainDiaries(@Param("year") int year, @Param("month") int month, @Param("userId") int userId);
+
+    Optional<Diary> findByUserAndDate(User user, Date date);
 }
